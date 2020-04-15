f029ba14d90666f03de590ac5d9218f331ff098e
Iann Ortnau <iann_ortnau@hotmail.com>
2020-04-15 19:36:24 -0300

    deletando copias

--- conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
- $link = mysqli_connect('localhost', 'root', '', 'rpgplayer');
-    mysqli_query($link, "SET NAMES 'utf8'");
-    mysqli_query($link, 'SET character_set_connection=utf8');
-    mysqli_query($link, 'SET character_set_client=utf8');
-    mysqli_query($link, 'SET character_set_results=utf8');
-
-?>
